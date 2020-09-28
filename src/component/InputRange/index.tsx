2746f1e209eb4ca87ff1a5c15eac31064c90651f
Mirudistok <mirudistok32@yandex.ru>
2020-09-28 22:44:09 +0300

    Refactor styles in InputRange component

--- src/component/InputRange/index.tsx
+++ src/component/InputRange/index.tsx
@@ -1,26 +1,6 @@
 import React, { ChangeEvent } from 'react'
 import s from './InputRange.module.scss'
 
-
-const setStyleLeftOutput = (valuedefault: number) => {
-    let result = -1
-    result = valuedefault + result
-    if (valuedefault > 5 && valuedefault <= 10) {
-        result = valuedefault - 3
-    } else if (valuedefault > 10 && valuedefault <= 30) {
-        result = valuedefault - 5
-    } else if (valuedefault > 30 && valuedefault <= 50) {
-        result = valuedefault - 8
-    } else if (valuedefault > 50 && valuedefault <= 75) {
-        result = valuedefault - 10
-    } else if (valuedefault > 75 && valuedefault <= 91) {
-        result = valuedefault - 12
-    } else if (valuedefault > 91 && valuedefault <= 107) {
-        result = valuedefault - 14
-    }
-    return result + "%"
-}
-
 type PropsType = {
     className?: string
     value: number
@@ -37,7 +17,7 @@ export const InputRange: React.FC<PropsType> = React.memo((props) => {
 
     return (
         <div className={mainStyles.join(" ")}>
-            <output className={s.wrap__output} style={{ left: setStyleLeftOutput(value) }}>
+            <output className={s.wrap__output} >
                 {
                     value
                 }
