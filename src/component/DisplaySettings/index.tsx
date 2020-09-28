ddadc4e616f2bf8285d34e4f2b76ac400334bb4e
Mirudistok <mirudistok32@yandex.ru>
2020-09-28 18:57:58 +0300

    DisplaySettings

---
+++ src/component/DisplaySettings/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './DisplaySettings.module.scss'
+
+export const DisplaySettings = () => {
+
+
+    return (
+        <div className={s.displaysettings}>
+            
+        </div>
+    )
+}
