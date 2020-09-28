f354a9b58b5b1bac1e24ad68d36b698fb762790e
Mirudistok <mirudistok32@yandex.ru>
2020-09-28 19:25:26 +0300

    Create Button component

---
+++ src/component/Button/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './Button.module.scss'
+
+export const Button = () => {
+
+    
+    return (
+        <div className={s.btn}>
+
+        </div>
+    )
+}
--- src/component/DisplayPassword/index.tsx
+++ src/component/DisplayPassword/index.tsx
@@ -3,10 +3,18 @@ import s from './DisplayPassword.module.scss'
 
 export const DisplayPassword = () => {
 
+    let arr = []
+    for (let i = 0; i < 107; i++) {
+        arr.push(i)
+    }
+    const renderSymbols = arr.map((i, idx) => <span key={idx + 1}>{Math.floor(Math.random() * 1000)}</span>)
+
 
     return (
-        <div className={s.displaypassword} contentEditable>
-            
+        <div className={s.displaypassword}>
+            {
+                renderSymbols
+            }
         </div>
     )
 }
