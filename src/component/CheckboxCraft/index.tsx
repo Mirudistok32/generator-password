92341f47de90080c366ebb16ed1b9e72b5d50971
Mirudistok <mirudistok32@yandex.ru>
2020-09-29 11:25:39 +0300

    Create CheckboxCraft component

---
+++ src/component/CheckboxCraft/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import s from './CheckboxCraft.module.scss'
+
+type PropsType = {
+    className?: string
+    title: string
+}
+
+export const CheckboxCraft: React.FC<PropsType> = (props) => {
+
+    const { className, title } = props
+
+    return (
+        <label className={s.checkbox}>
+            <span className={s.checkbox__title}>{title}</span>
+            <input className={s.checkbox__input} type="checkbox" />
+        </label>
+    )
+}
