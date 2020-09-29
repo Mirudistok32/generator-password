2677999578594264f56ff017e6fc56ffa4f9d6a9
Mirudistok32 <mirudistok32@yandex.ru>
2020-09-29 20:51:41 +0300

    Create Footer component

--- src/component/App/index.tsx
+++ src/component/App/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Boxdisplay } from '../Boxdisplay';
+import { Footer } from '../Footer';
 import { Title } from '../Title';
 import s from './App.module.scss';
 
 export const App = () => {
   return (
     <div className={s.app}>
-      <Title title="Generator password" />
-      <Boxdisplay />
+      <div className={s.app__wrap}>
+        <Title title="Generator password" />
+        <Boxdisplay />
+      </div>
+      <Footer className={s.app__footer}/>
     </div>
   );
 }
