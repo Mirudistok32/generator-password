91d59b626fead49a56f8346766ab19f32c3ad31a
Mirudistok <mirudistok32@yandex.ru>
2020-09-29 12:02:56 +0300

    Add styles for craft input in CheckboxCraft

--- src/component/CheckboxCraft/index.tsx
+++ src/component/CheckboxCraft/index.tsx
@@ -4,16 +4,17 @@ import s from './CheckboxCraft.module.scss'
 type PropsType = {
     className?: string
     title: string
+    disabled?: boolean
 }
 
 export const CheckboxCraft: React.FC<PropsType> = React.memo((props) => {
 
-    const { className, title } = props
+    const { className, title, disabled } = props
 
     return (
         <label className={s.checkbox + `${className ? ' ' + className : ''}`}>
             <span className={s.checkbox__title}>{title}</span>
-            <input className={s.checkbox__input} type="checkbox" />
+            <input className={s.checkbox__input} type="checkbox" disabled={disabled} />
             <span className={s['checkbox__input-craft']}></span>
         </label>
     )
